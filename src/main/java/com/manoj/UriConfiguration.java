7caba537e369b079b06cccf9240074c85142c918
manoj.dhayalan <manoj.dhayalan@mapp.com>
2025-05-21 09:39:30 +0200

    Refactor code

--- src/main/java/com/manoj/UriConfiguration.java
+++ src/main/java/com/manoj/UriConfiguration.java
@@ -6,11 +6,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class UriConfiguration {
     private String httpbin = "http://httpbin.org:80";
 
-  public String getHttpbin() {
-    return httpbin;
-  }
+    public String getHttpbin() {
+        return httpbin;
+    }
 
-  public void setHttpbin(String httpbin) {
-    this.httpbin = httpbin;
-  }
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
 }
--- src/test/java/com/manoj/ApiGatewayTests.java
+++ src/test/java/com/manoj/ApiGatewayTests.java
@@ -10,40 +10,40 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+        properties = {"httpbin=http://localhost:${wiremock.server.port}"})
 @AutoConfigureWireMock(port = 0)
 class ApiGatewayTests {
 
-	@Autowired
-  private WebTestClient webClient;
+    @Autowired
+    private WebTestClient webClient;
 
-  @Test
-  public void contextLoads() throws Exception {
-    //Stubs
-    stubFor(get(urlEqualTo("/get"))
-        .willReturn(aResponse()
-          .withBody("{\"headers\":{\"Hello\":\"World\"}}")
-          .withHeader("Content-Type", "application/json")));
-    stubFor(get(urlEqualTo("/delay/3"))
-      .willReturn(aResponse()
-        .withBody("no fallback")
-        .withFixedDelay(3000)));
+    @Test
+    public void contextLoads() throws Exception {
+        //Stubs
+        stubFor(get(urlEqualTo("/get"))
+                .willReturn(aResponse()
+                        .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                        .withHeader("Content-Type", "application/json")));
+        stubFor(get(urlEqualTo("/delay/3"))
+                .willReturn(aResponse()
+                        .withBody("no fallback")
+                        .withFixedDelay(3000)));
 
-    webClient
-      .get().uri("/get")
-      .exchange()
-      .expectStatus().isOk()
-      .expectBody()
-      .jsonPath("$.headers.Hello").isEqualTo("World");
+        webClient
+                .get().uri("/get")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.headers.Hello").isEqualTo("World");
 
-    webClient
-      .get().uri("/delay/3")
-      .header("Host", "www.circuitbreaker.com")
-      .exchange()
-      .expectStatus().isOk()
-      .expectBody()
-      .consumeWith(
-        response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
-  }
+        webClient
+                .get().uri("/delay/3")
+                .header("Host", "www.circuitbreaker.com")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(
+                        response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
+    }
 
 }
