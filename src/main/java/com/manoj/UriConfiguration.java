5d0a98b5e2b976a858b42ea899dc1e421ac7263a
manoj.dhayalan <manoj.dhayalan@mapp.com>
2025-05-21 09:58:09 +0200

    Refactor code

--- src/main/java/com/manoj/UriConfiguration.java
+++ src/main/java/com/manoj/UriConfiguration.java
@@ -4,13 +4,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties
 public class UriConfiguration {
-    private String httpbin = "http://httpbin.org:80";
+  private String httpbin = "http://httpbin.org:80";
 
-    public String getHttpbin() {
-        return httpbin;
-    }
+  public String getHttpbin() {
+    return httpbin;
+  }
 
-    public void setHttpbin(String httpbin) {
-        this.httpbin = httpbin;
-    }
+  public void setHttpbin(String httpbin) {
+    this.httpbin = httpbin;
+  }
 }
